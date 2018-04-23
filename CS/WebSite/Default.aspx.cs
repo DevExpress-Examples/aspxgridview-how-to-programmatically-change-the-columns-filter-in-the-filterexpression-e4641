e9c3d60495850fb49dc95918c2455001127b49b1
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/WebSite/Default.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using DevExpress.Data.Filtering;
+using DevExpress.Web;
+
+public partial class _Default : System.Web.UI.Page {
+
+    protected void ASPxGridView1_CustomCallback(object sender, ASPxGridViewCustomCallbackEventArgs e) {
+        ASPxGridView grid = sender as ASPxGridView;
+        ModifyFilterExpression(ASPxComboBox1.Value.ToString(), ASPxTextBox1.Value, grid);
+    }
+    protected void ModifyFilterExpression(string FieldName, object value, ASPxGridView victim) {
+        var criterias = CriteriaColumnAffinityResolver.SplitByColumns(CriteriaOperator.Parse(victim.FilterExpression));
+
+        CriteriaOperator co = null;
+        if (FieldName == "ProductName") {
+            value += "%";
+            co = new FunctionOperator("Like", new OperandProperty(FieldName), new OperandValue(value));
+        } else
+            co = new BinaryOperator(FieldName, value, BinaryOperatorType.Equal);
+        if (!criterias.Keys.Contains(new OperandProperty(FieldName)))
+            criterias.Add(new OperandProperty(FieldName), co); 
+        else
+            criterias[new OperandProperty(FieldName)] = co; 
+        victim.FilterExpression = CriteriaOperator.ToString(GroupOperator.And(criterias.Values));
+    }
+
+}
---
+++ VB/WebSite/Default.aspx.vb
@@ -0,0 +1,32 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports DevExpress.Data.Filtering
+Imports DevExpress.Web
+
+Partial Public Class _Default
+    Inherits System.Web.UI.Page
+
+    Protected Sub ASPxGridView1_CustomCallback(ByVal sender As Object, ByVal e As ASPxGridViewCustomCallbackEventArgs)
+        Dim grid As ASPxGridView = TryCast(sender, ASPxGridView)
+        ModifyFilterExpression(ASPxComboBox1.Value.ToString(), ASPxTextBox1.Value, grid)
+    End Sub
+    Protected Sub ModifyFilterExpression(ByVal FieldName As String, ByVal value As Object, ByVal victim As ASPxGridView)
+        Dim criterias = CriteriaColumnAffinityResolver.SplitByColumns(CriteriaOperator.Parse(victim.FilterExpression))
+
+        Dim co As CriteriaOperator = Nothing
+        If FieldName = "ProductName" Then
+            value &= "%"
+            co = New FunctionOperator("Like", New OperandProperty(FieldName), New OperandValue(value))
+        Else
+            co = New BinaryOperator(FieldName, value, BinaryOperatorType.Equal)
+        End If
+        If Not criterias.Keys.Contains(New OperandProperty(FieldName)) Then
+            criterias.Add(New OperandProperty(FieldName), co)
+        Else
+            criterias(New OperandProperty(FieldName)) = co
+        End If
+        victim.FilterExpression = CriteriaOperator.ToString(GroupOperator.And(criterias.Values))
+    End Sub
+
+End Class
