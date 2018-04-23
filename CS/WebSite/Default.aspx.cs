b75483c94f1d7a6417664da94885f78a3b604aaf
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/WebSite/Default.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using DevExpress.Data.Filtering;
+using DevExpress.Web.ASPxGridView;
+
+public partial class _Default : System.Web.UI.Page {
+
+    protected void ASPxGridView1_CustomCallback(object sender, ASPxGridViewCustomCallbackEventArgs e) {
+        ASPxGridView grid = sender as ASPxGridView;
+        ModifyFilterExpression(ASPxComboBox1.Value.ToString(), ASPxTextBox1.Value, grid);
+    }
+    protected void ModifyFilterExpression(string FieldName, object value, ASPxGridView victim) {
+        var criterias = CriteriaColumnAffinityResolver.SplitByColumns(CriteriaOperator.Parse(victim.FilterExpression));
+                
+        BinaryOperatorType operatorType;
+        if (FieldName == "ProductName") {
+            operatorType = BinaryOperatorType.Like;
+            value += "%";
+        } else
+            operatorType = BinaryOperatorType.Equal;
+
+
+        if (!criterias.Keys.Contains(new OperandProperty(FieldName)))
+            criterias.Add(new OperandProperty(FieldName), new BinaryOperator(FieldName, value, operatorType));
+        else
+            criterias[new OperandProperty(FieldName)] = new BinaryOperator(FieldName, value, operatorType);
+        victim.FilterExpression = CriteriaOperator.ToString(GroupOperator.And(criterias.Values));
+    }
+
+}
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# ASPxGridView - How to programmatically change the column's filter in the FilterExpression
+
+
+<p>This example demonstrates how to achieve this task. The CriteriaColumnAffinityResolver.SplitByColumns method is used here to return the dictionary of criteria operators. It is possible to modify them as necessary. Please refer to the <a href="https://www.devexpress.com/Support/Center/p/KA18784">ASPxGridView - How to programmatically change the column's filter in the FilterExpression</a> article for more information.</p>
+
+<br/>
+
+
